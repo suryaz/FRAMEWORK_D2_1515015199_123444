f7f9afaebd0bf589711c3d9f5f8ec4d5b77b8ee5
Surya Zulfiyahya <suryazulfiyahya4@gmail.com>
2017-03-11 22:17:43 -0800

    posttest2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hello-world',function(){
+	return 'Hello World';
+});
+
+Route::get('pengguna/{pengguna}',function($pengguna){
+	return "Hello World dari pengguna $pengguna";
+});
+
+Route::get('kelasd2/framework/{mhs?}',function($mhs="Anonim"){
+	return "Selamat Datang $mhs";
+});
